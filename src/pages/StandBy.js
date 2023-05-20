d28cde20f767fee04889aae6a15250d09f0fb5bd
ITKOO <itkoo2000@gmail.com>
2023-05-21 05:49:34 +0900

    feat(#12): 멘토멘티 결과 대기화면 퍼블리싱 구현

--- src/pages/StandBy.js
+++ src/pages/StandBy.js
@@ -3,12 +3,16 @@ import '../css/StandBy.css';
 
 const StandBy = () => {
   return (
-    <div className="">
-      <p>
-        멘티 · 멘토 매칭을 위해
-        커넥트 DKU봇이
-        열심히 일하고 있어요!
-      </p>
+    <div className="page ftM">
+      <div className="standby">
+        <p className="boldTxt textCenter wideLineHeight">
+          멘티 · 멘토 매칭을 위해<br/>
+          <span className="blueTxt">커넥트 DKU봇</span>이<br/>
+          열심히 일하고 있어요!
+        </p>
+        <img src="/img/loading.gif" alt="" className="standbyLoading"/>
+        <button className="btnPrimary standbyBtn">결과 확인하러 가기</button>
+      </div>
     </div>
   );
 };
