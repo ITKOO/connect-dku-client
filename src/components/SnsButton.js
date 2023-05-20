f43c57d2f8af97fac27da3f6410b5374ab325b5c
ITKOO <itkoo2000@gmail.com>
2023-05-21 04:42:44 +0900

    test(#2): 카카오 로그인 테스트

--- src/components/SnsButton.js
+++ src/components/SnsButton.js
@@ -6,9 +6,17 @@ const SnsButton = ({type}) => {
   const buttonImgUrl = type === 'KAKAO' ? 'kakao_logo.png' : 'naver_logo.png';
   const buttonText = type === 'KAKAO' ? '카카오' : '네이버';
 
+  // TODO 추후 config 파일 분리
+  const REST_API_KEY = 'e0c2a8eb689189ed5aee477f8677df37';
+  const REDIRECT_URL = 'http://localhost:3000';
+  const kakaoLoginUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URL}&response_type=code`;
+  const loginWithKakao = () => {
+    window.location.href = kakaoLoginUrl
+  }
+
   return (
     <div>
-      <button className={buttonColor}>
+      <button className={buttonColor} onClick={loginWithKakao}>
         <img src={`../../img/${buttonImgUrl}`} alt="sns 이미지"/>
         {buttonText}로 로그인하기
       </button>
